5f74a77db0ad037ec10cc437b52b126340a254f8
Rhea Iyer <rhea.iyer8@gmail.com>
2022-10-23 05:08:27 -0400

    feed

--- feed.js
+++ feed.js
@@ -7,6 +7,20 @@ var mail = "rhea.iyer8@gmail.com";
 var phone = "(888) 888-8888";
 var employee = "Rhea Iyer";
 var position =  "Co-Founder";
+const tags1 = ["Hospitality", "Catering"]
+const tags2 = ["Finance", "Banking"]
+const tags3 = ["Information Technology", "Computer Science"]
+
+
+let hosp = document.getElementById("industry1");
+let IT = document.getElementById("industry2");
+let homeCare = document.getElementById("industry3");
+let finance = document.getElementById("industry4");
+
+
+let bus1 = document.getElementById("bus1");
+let bus2 = document.getElementById("bus2");
+let bus3 = document.getElementById("bus3");
 
 
 function docWrite(variable) {
@@ -14,10 +28,51 @@ function docWrite(variable) {
 
 }
 
-let feed = document.getElementById("feed");
-let microLoans = document.getElementById("microLoans");
-let addResources = document.getElementById("addResources");
-let contactUs = document.getElementById("contactUs");
+function changeVis1(){
+    alert("hallo");
+    bus3.style.display = "none";
+    if(document.getElementById("id1").checked == true){
+        bus1.style.display = "none";
+        bus1.style.visibility = "hidden";
+
+        if(tags1.includes("Hospitality")){
+            setToVisible(bus1);
+
+        }
+        else{
+            setToHidden(bus1);
+        }
+        if(tags2.includes("Hospitality")){
+            setToVisible(bus2)
+        }
+        else{
+            setToHidden(bus2);
+        }
+        if(tags2.includes("Hospitality")){
+            setToVisible(bus3)
+        }
+        else{
+            setToHidden(bus3);
+        }
+        
+    }
+    return;
+    
+    
+
+}
+
+function setToHidden(bus){
+    bus.style.visibility = "hidden";
+    return;
+}
+
+function setToVisible(bus){
+    bus.style.visibility = "visible";
+    return;
+}
+
+
 
 
 
